bf88e7e0b4d95fd0366baebc060248586fd75886
preshu1 <shresthapreshu26@gmail.com>
2025-11-27 00:06:09 +0545

    displayed the data on the ui

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,8 @@
-//const weatherResult = document.getElementById("weatherResult");
+//selecting
+const cityInput = document.getElementById("cityInput");
+const getWeatherbtn = document.getElementById("getWeatherbtn");
+const weatherResult = document.getElementById("weatherResult");
+const resetBtn = document.getElementById("reset");
 
 //fetching api that converts the city name to altitude and longitude values
 async function getWeather(cityName) {
@@ -9,7 +13,7 @@ async function getWeather(cityName) {
 
     //checking if the city name exists or not + validating
     if (!geoData.results || geoData.results.length === 0) {
-      console.log("City not found!");
+      weatherResult.innerHTML = `<p>City Not Found!</p>`;
       return;
     }
     const { latitude, longitude, name, country } = geoData.results[0];
@@ -18,14 +22,48 @@ async function getWeather(cityName) {
     const weatherUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`;
     const weatherRes = await fetch(weatherUrl);
     const weatherData = await weatherRes.json();
+    const weather = weatherData.current_weather;
 
-    //displaying the result
-    console.log(`City: ${name},${country}`);
-    console.log(`Temperature: ${weatherData.current_weather.temperature}°C`);
-    console.log(`Wind: ${weatherData.current_weather.windspeed} km/h`);
+    //displaying weather dynamically using map and join method
+    const weatherInfo = Object.entries(weather)
+      .map(([Key, value]) => {
+        return `<div class="weather-item"><strong>${Key}</strong>: ${value}</div>`;
+      })
+      .join("");
+
+    //show city name and country
+    weatherResult.innerHTML = `<h2 class="heading">Weather in ${name}, ${country}</h2>${weatherInfo}`;
+    cityInput.value = "";
+    cityInput.focus();
+
+    //hide input and button to focus on result
+    resetBtn.style.display = "block";
+    cityInput.style.display = "none";
+    getWeatherbtn.style.display = "none";
   } catch (err) {
-    console.log("Error!", err);
+    weatherResult.innerHTML = `<p>Error Fetching Weather</p>`;
+    console.error(err);
   }
 }
 
-getWeather("kathmandu");
+//button click event
+getWeatherbtn.addEventListener("click", async () => {
+  const city = cityInput.value;
+  if (!city) {
+    weatherResult.innerHTML = `<p style="color: red; font-size: 18px;">Please enter a city</p>`;
+
+    cityInput.focus();
+    return;
+  }
+
+  getWeather(city);
+});
+
+resetBtn.addEventListener("click", () => {
+  weatherResult.innerHTML = ""; // clear the weather card
+  cityInput.value = ""; // clear input
+  cityInput.style.display = "inline-block"; //  hidden earlier
+  getWeatherbtn.style.display = "inline-block"; //  hidden earlier
+  cityInput.focus(); // focus input
+  resetBtn.style.display = "none";
+});
